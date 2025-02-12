ec4fe04725ca3fa4c5db53035b61c01fb1e22106
Martin Doyle <martindoyle96@gmail.com>
2025-02-12 09:37:07 +0000

    Initial Commit

---
+++ LeadEssentials/APIClient.swift
@@ -0,0 +1,17 @@
+//
+//  APIClient.swift
+//  LeadEssentials
+//
+//  Created by Martin Doyle on 11/02/2025.
+//
+
+import Foundation
+
+class APIClient {
+    static let instance = APIClient()
+    private init() {}
+}
+
+let client = APIClient.instance
+
+
---
+++ LeadEssentials/LeadEssentials.h
@@ -0,0 +1,18 @@
+//
+//  LeadEssentials.h
+//  LeadEssentials
+//
+//  Created by Martin Doyle on 11/02/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeadEssentials.
+FOUNDATION_EXPORT double LeadEssentialsVersionNumber;
+
+//! Project version string for LeadEssentials.
+FOUNDATION_EXPORT const unsigned char LeadEssentialsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeadEssentials/PublicHeader.h>
+
+
